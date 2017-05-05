39015a3e74ce8a804a9fbfdbe215085b3da7f6cf
David W <earlofsalad@gmail.com>
2017-05-05 13:17:07 -0700

    Appropriate out- Put in unordered list. Wrong format, fix next
    add and re-arrange backend logic to account for pinging and ponging, and even some ping-ponging. Output array is correct, have to fix output format next.

--- js/scripts.js
+++ js/scripts.js
@@ -1,25 +1,28 @@
 //------------------------------------- BUSINESS LOGIC
-var soloResult = function(soloInput) {
-  debugger;
-  var soloInputArray = [];
-  var i = 0;
-  for(i; i <= soloInput; i++){
-    // soloInputArray.push(i);
-    if (((soloInput % 15) === 0) && (soloInput !== 3 || soloInput !== 5)){
-      soloInputArray.push("ping-pong");
+var soloInputArray = function(soloInput) {
+  var testArray = [];
+  for(var i = 1; i <= soloInput; i ++){
+    testArray.push(i);
+  }
+  var resultArray = testArray.map(function(i){
+    
+    if(((i % 15) === 0) && (i !== 3 || i !== 5)){
+      return "ping-pong";
     }
-    else if((soloInput % 5) === 0) {
-      soloInputArray.push("pong");
+    else if((i % 5) === 0) {
+      return "pong";
     }
-    else if((soloInput % 3) === 0){
-      soloInputArray.push("ping");
+    else if ((i % 3) === 0){
+      return "ping";
     }
     else {
-      soloInputArray.push(i);
+      return i;
     }
-  }
-  return soloInputArray;
+  });
+  console.log(resultArray);
+  return resultArray;
 };
+
 
 
 //------------------------------------- USER INTERFACE LOGIC
@@ -27,6 +30,6 @@ $(function(){
   $("#soloForm").submit(function(event){
     event.preventDefault();
     var soloInput = parseInt($("input#soloInput").val());
-    $("#output").append(soloResult(soloInput));
+    $("ul#output").append("<li>" + soloInputArray(soloInput) + "</li>");
   })
 })
